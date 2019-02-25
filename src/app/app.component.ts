ea610f849b495c8389762ad79497a75cb2ed4618
dario <fruitprojectsrl@gmail.com>
2019-02-25 12:20:49 +0100

    (validazione e routing modulo ok)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'progettosito';
 }
+$(function(){
+$('#welcome')
+  .animate({width:'50%'}, {queue:false, duration: 3000})
+  .animate({fontSize:'100px'}, 3000)
+  .animate({borderLeftWidth:'1000px'}, 2000);
+
+});
+
+$(function(){
+$("#immagini").on('mouseover',function(){
+setTimeout(function(){
+			$('#chitarra').fadeIn()},500);
+		setTimeout(function(){
+		$('#sax').fadeIn()},1000);
+		setTimeout(function(){
+		$('#piano').fadeIn()},1500);
+
+	});
+});
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ModuloComponent } from './modulo/modulo.component';
+import { HttpClientModule } from '@angular/common/http'; 
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ModuloComponent
   ],
   imports: [
     ReactiveFormsModule,
@@ -28,7 +30,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
